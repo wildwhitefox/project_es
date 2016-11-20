e459734acdca67fa241d15a40498228238a0646f
wildwhitefox <alan.thomaz@usp.br>
2016-11-20 19:49:28 -0200

    ...

--- Test/phpunit/Usuario/DeletarTest.php
+++ Test/phpunit/Usuario/DeletarTest.php
@@ -1,12 +1,6 @@
 <?php
 
 class DeletarTest extends PHPUnit_Framework_TestCase {
-    protected function setUp()
-          addUsuario("valido", "valido  jacinto","senhasupervalidaesegura","valido@valido.com",36660666);
-
-    protected function tearDown(){
-        remove();
-    }
     public function testDeletaInvalido(){
         $this->assertEquals(-1, deleta(null));
     }
